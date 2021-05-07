a1a23cf20cdfa05a65b994597aedbeb884e98636
Tsagae <matteo2309@hotmail.com>
2021-05-07 13:09:11 +0200

    updated db interface service

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,24 +1,23 @@
-const loginData = require('./loginData');
-var mysql = require('mysql');
-
+const loginData = require("./loginData");
+const mysql = require("mysql");
 
 var con = mysql.createConnection({
-	host: loginData.host,
-	user: loginData.user,
-	password: loginData.password,
-	database: loginData.database		
+  host: loginData.host,
+  user: loginData.user,
+  password: loginData.password,
+  database: loginData.database,
 });
 
-var sql = "SELECT getIdByGameName('Age of Empires');"
+var sql = "SELECT getIdByGameName('Age of Empires');";
 //var sql = "SELECT * FROM All_Games;"
 
-con.connect(function(err){
-	if(err) throw err;
-	console.log("Connected!");
-	con.query(sql, function(err, result, fields){
-		 if(err) throw err;
-		 console.log("Result: " + JSON.stringify(result));
-		 console.log();
-		 process.exit(0);
-	});
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    console.log("Result: " + JSON.stringify(result));
+    console.log();
+    process.exit(0);
+  });
 });
--- services/service2.service.js
+++ services/service2.service.js
@@ -1,33 +1,28 @@
 const express = require("express");
-var MySqlInterface = require("./MySqlInterface.service");
-const loginData = require("../loginData");
-const mysql = require("mysql");
-
-function execQuery(sql) {
-  var outResult;
-  let con = mysql.createConnection({
-    host: loginData.host,
-    user: loginData.user,
-    password: loginData.password,
-    database: loginData.database,
-  });
-
-  con.connect(function (err) {
-    if (err) outResult = err; //TODO remove for production
-    console.log("Connected!");
-    con.query(sql, function (err, result, fields) {
-      if (err) outResult = err; //TODO remove for production
-      outResult = JSON.stringify(result);
-      console.log(outResult);
-    }); 
-  });
-  return outResult;
-}
 
 module.exports = {
   getList: (req, res) => {
-    let list = execQuery("SELECT * FROM All_Games LIMIT 100");
-    console.log(list);
-    return res.status(200).json({ list: list });
+    const loginData = require("../loginData");
+    const mysql = require("mysql");
+
+    var con = mysql.createConnection({
+      host: loginData.host,
+      user: loginData.user,
+      password: loginData.password,
+      database: loginData.database,
+    });
+
+    var sql = "SELECT getIdByGameName('Age of Empires');";
+    //var sql = "SELECT * FROM All_Games;"
+
+    con.connect(function (err) {
+      if (err) return err; //TODO remove for production
+      console.log("Connected!");
+      con.query(sql, function (err, result, fields) {
+        if (err) return err; //TODO remove for production
+        console.log("Result: " + JSON.stringify(result));
+        return res.status(200).json(JSON.stringify(result));
+      });
+    });
   },
 };
