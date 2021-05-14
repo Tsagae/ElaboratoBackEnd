5a7fa600d76d00d8ba3acbac24f23f4c25c68c34
Tsagae <matteo2309@hotmail.com>
2021-05-14 14:27:49 +0200

    added  cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { query } = require('express');
 const express = require('express');
+var cors = require('cors');
 const app = express();
 
 const menu = require('./routes/menu');
 const service2 = require('./routes/service2');
 const queryDB = require('./routes/queryDB');
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/menu', menu);
