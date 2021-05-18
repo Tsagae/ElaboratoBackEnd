fd2470a5bc098431c6400efd68acb867da1104d9
Tsagae <matteo2309@hotmail.com>
2021-05-18 22:35:18 +0200

    updated removed list from router queryDB

--- routes/queryDB.js
+++ routes/queryDB.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const queryDB = require('../services/queryDB.service');
 
-router.get('/', queryDB.getList);
 router.get('/getIdByGameName', queryDB.getIdByGameName);
 router.get('/getHistoricalEsportData_Post2016', queryDB.getHistoricalEsportData_Post2016);
 router.get('/getHoursWatched12Months', queryDB.getHoursWatched12Months);
