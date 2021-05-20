817df5beb3152556529fa30b6ca31fbc93ba789f
Tsagae <matteo2309@hotmail.com>
2021-05-20 12:51:44 +0200

    updated api name

--- routes/queryDB.js
+++ routes/queryDB.js
@@ -5,7 +5,7 @@ const queryDB = require('../services/queryDB.service');
 
 router.get('/getIdByGameName', queryDB.getIdByGameName);
 router.get('/getHistoricalEsportData_Post2016', queryDB.getHistoricalEsportData_Post2016);
-router.get('/getHoursWatched12Months', queryDB.getHoursWatched12Months);
+router.get('/getHoursWatched', queryDB.getHoursWatched);
 router.get('/getMostPlayed12Months', queryDB.getMostPlayed12Months);
 router.get('/getMostViewed12Months', queryDB.getMostViewed12Months);
 router.get('/getTwitchGlobal_HoursWatched', queryDB.getTwitchGlobal_HoursWatched);
