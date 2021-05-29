8873876c9c2ed9258eae789a508f31ec67ae4030
Tsagae <matteo2309@hotmail.com>
2021-05-29 19:05:54 +0200

    service 2 now smiles :D

--- services/service2.service.js
+++ services/service2.service.js
@@ -2,27 +2,6 @@ const express = require("express");
 
 module.exports = {
   getList: (req, res) => {
-    const loginData = require("../loginData");
-    const mysql = require("mysql");
-
-    var con = mysql.createConnection({
-      host: loginData.host,
-      user: loginData.user,
-      password: loginData.password,
-      database: loginData.database,
-    });
-
-    //var sql = "SELECT getIdByGameName('Age of Empires');";
-    var sql = "SELECT * FROM All_Games;"
-
-    con.connect(function (err) {
-      if (err) return err; //TODO remove for production
-      console.log("Connected!");
-      con.query(sql, function (err, result, fields) {
-        if (err) return err; //TODO remove for production
-        console.log("Result: " + JSON.stringify(result));
-        return res.status(200).json(JSON.stringify(result));
-      });
-    });
+    return res.status(200).json(":D");
   },
 };
